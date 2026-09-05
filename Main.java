fe2b5eddf8f419387cca172ccff2a4c78e491268
nsaksharacse2405a2-ux <nsakshara_cse2405a2@mgit.ac.in>
2026-09-05 11:01:39 +0530

    added cal

---
+++ Main.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter first number: ");
+        double a = sc.nextDouble();
+
+        System.out.print("Enter second number: ");
+        double b = sc.nextDouble();
+
+        System.out.println("Addition = " + (a + b));
+        System.out.println("Subtraction = " + (a - b));
+        System.out.println("Multiplication = " + (a * b));
+
+        if (b != 0) {
+            System.out.println("Division = " + (a / b));
+        } else {
+            System.out.println("Division not possible");
+        }
+
+        sc.close();
+    }
+} 
